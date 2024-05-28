dfdd6dad7bae51c5a05e37bbd134d56f0536f71a
azamat-110 <149684180+azamat-110@users.noreply.github.com>
2024-05-28 16:56:25 +0500

    Update cards.js

--- script/cards.js
+++ script/cards.js
@@ -2,42 +2,42 @@
 const integration = [
     {
         id: 1,
-        logo: '../assets/images/amazon_word.svg',
+        logo: './assets/images/amazon_word.svg',
         name: 'Amazon',
         type: 'Маркетплейсы',
         descr: 'Амазон — крупнейший маркетплейс в мире родом из США',
     },
     {
         id: 2,
-        logo: '../assets/images/ebay.svg',
+        logo: './assets/images/ebay.svg',
         name: 'Ebay',
         type: 'Маркетплейсы',
         descr: 'Ebay - cамый популярный интернет-аукцион в мире, работающий с 1995 года.',
     },
     {
         id: 3,
-        logo: '../assets/images/etsy.svg',
+        logo: './assets/images/etsy.svg',
         name: 'Etsy',
         type: 'Маркетплейсы',
         descr: 'Etsy - международная торговая площадка, специализирующаяся на продаже уникальных изделий.',
     },
     {
         id: 4,
-        logo: '../assets/images/Yandex-market.svg',
+        logo: './assets/images/Yandex-market.svg',
         name: 'Яндекс.Маркет',
         type: 'Маркетплейсы',
         descr: 'Старейший российский маркетплейс потребительских товаров',
     },
     {
         id: 5,
-        logo: '../assets/images/avito.svg',
+        logo: './assets/images/avito.svg',
         name: 'Avito',
         type: 'Маркетплейсы',
         descr: 'Avito - самый популярный в России онлайн-сервис для продажи товаров, насчитывающий более 50 млн активных объявлений.',
     },
     {
         id: 6,
-        logo: '../assets/images/Russian_Post.svg',
+        logo: './assets/images/Russian_Post.svg',
         name: 'Почта России',
         type: 'Логистические интеграции',
         descr: 'Интеграция с сервисами Почты России для международной интернет-торговли. ',
@@ -45,7 +45,7 @@ const integration = [
     },
     {
         id: 7,
-        logo: '../assets/images/DHL_Logo.svg',
+        logo: './assets/images/DHL_Logo.svg',
         name: 'DHL',
         type: 'Логистические интеграции',
         descr: 'Модуль позволяет создать интеграцию с DHL, осуществляющей доставку грузов по всему миру.',
@@ -53,7 +53,7 @@ const integration = [
     },
     {
         id: 8,
-        logo: '../assets/images//shiptor.svg',
+        logo: './assets/images//shiptor.svg',
         name: 'Shiptor',
         type: 'Логистические интеграции',
         descr: 'Модуль позволяет сделать интеграцию со службой доставки Shiptor.',
@@ -61,7 +61,7 @@ const integration = [
     },
     {
         id: 9,
-        logo: '../assets/images/Bitrix24-logo-ru.svg',
+        logo: './assets/images/Bitrix24-logo-ru.svg',
         name: 'Битрикс-24',
         type: 'CRM / CMS',
         descr: 'Битрикс24 — российский сервис для управления бизнесом.',
@@ -69,7 +69,7 @@ const integration = [
     },
     {
         id: 10,
-        logo: '../assets/images/1c_bitrix_logo.svg',
+        logo: './assets/images/1c_bitrix_logo.svg',
         name: '1С-Битрикс',
         type: 'CRM / CMS',
         descr: '1С-Битрикс — российский сервис для управления бизнесом.',
@@ -77,7 +77,7 @@ const integration = [
     },
     {
         id: 11,
-        logo: '../assets/images/retail.svg',
+        logo: './assets/images/retail.svg',
         name: 'Retail-CRM',
         type: 'CRM / CMS',
         descr: 'Retail-CRM —  сервис для управления бизнесом.',
@@ -85,7 +85,7 @@ const integration = [
     },
     {
         id: 12,
-        logo: '../assets/images/inSales.svg',
+        logo: './assets/images/inSales.svg',
         name: 'InSales',
         type: 'CRM / CMS',
         descr: 'InSales — сервис для управления бизнесом.',
